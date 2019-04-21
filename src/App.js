7124c94278afd5545ac273293769e09e43511fe1
Tito <tito.parra4@@hotmail.com>
2019-04-21 19:43:38 +0200

    Consulting the API to obtain events

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Formulario from './componentes/Formulario';
 
 class App extends Component {
 	token = 'FVWKU4EOG57CWT4S73EL';
+	ordenar = 'date';
 
 	state = {
-		categorias: []
+		categorias: [],
+		eventos: []
 	};
 
 	componentDidMount() {
@@ -16,18 +18,21 @@ class App extends Component {
 	obtenerCategorias = async () => {
 		let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}`;
 
+		obtenerEventos = async (busqueda) => {
+			let url = `https://www.eventbriteapi.com/v3/events/search/?q=${busqueda.nombre}&formats=${busqueda.categoria}&sort_by=${this
+				.ordenar}&token=${this.token}`;
+		};
+
 		await fetch(url)
 			.then((respuesta) => {
 				return respuesta.json();
 			})
-			.then((categorias) => {
+			.then((eventos) => {
 				this.setState({
-					categorias: categorias.categories
+					eventos: eventos.events
 				});
 			});
 	};
-
-	obtenerEventos = async (busqueda) => {};
 
 	render() {
 		return (

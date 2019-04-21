c7625d31f234b1c56f60b366821786d1b28bef48
Tito <tito.parra4@@hotmail.com>
2019-04-21 18:30:19 +0200

    Saving the categories in the state

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 import Header from './componentes/Header';
 
 class App extends Component {
+	token = 'FVWKU4EOG57CWT4S73EL';
+
+	state = {
+		categorias: []
+	};
+
 	componentDidMount() {
 		this.obtenerCategorias();
 	}
 
 	obtenerCategorias = async () => {
-		let url = `https://www.eventbriteapi.com/v3/categories/?token=FVWKU4EOG57CWT4S73EL`;
+		let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}`;
 
 		await fetch(url)
 			.then((respuesta) => {
 				return respuesta.json();
 			})
 			.then((categorias) => {
-				console.log(categorias);
+				this.setState({
+					categorias: categorias.categories
+				});
 			});
 	};
 

05edd24c036230eb4fb289d2ba445b481cf7f5aa
Tito <tito.parra4@@hotmail.com>
2019-04-21 20:25:25 +0200

    Creating Components to show events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario';
+import Eventos from './componentes/Eventos';
 
 class App extends Component {
 	token = 'FVWKU4EOG57CWT4S73EL';
@@ -18,10 +19,20 @@ class App extends Component {
 	obtenerCategorias = async () => {
 		let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}`;
 
-		obtenerEventos = async (busqueda) => {
-			let url = `https://www.eventbriteapi.com/v3/events/search/?q=${busqueda.nombre}&formats=${busqueda.categoria}&sort_by=${this
-				.ordenar}&token=${this.token}`;
-		};
+		await fetch(url)
+			.then((respuesta) => {
+				return respuesta.json();
+			})
+			.then((categorias) => {
+				this.setState({
+					categorias: categorias.categories
+				});
+			});
+	};
+
+	obtenerEventos = async (busqueda) => {
+		let url = `https://www.eventbriteapi.com/v3/events/search/?q=${busqueda.nombre}&categories=${busqueda.categoria}&sort_by=${this
+			.ordenar}&token=${this.token}`;
 
 		await fetch(url)
 			.then((respuesta) => {
@@ -41,6 +52,8 @@ class App extends Component {
 
 				<div className="uk-container">
 					<Formulario categorias={this.state.categorias} obtenerEventos={this.obtenerEventos} />
+
+					<Eventos eventos={this.state.eventos} />
 				</div>
 			</div>
 		);
---
+++ src/componentes/Evento.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Evento = (props) => {
+	return <p>From evento js</p>;
+};
+
+export default Evento;

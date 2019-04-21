6e24a1ae42d1b41f77c1337544d8b9063e9d9e3a
Tito <tito.parra4@@hotmail.com>
2019-04-21 18:24:55 +0200

    Getting API categories

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import Header from './componentes/Header';
 
 class App extends Component {
+	componentDidMount() {
+		this.obtenerCategorias();
+	}
+
+	obtenerCategorias = async () => {
+		let url = `https://www.eventbriteapi.com/v3/categories/?token=FVWKU4EOG57CWT4S73EL`;
+
+		await fetch(url)
+			.then((respuesta) => {
+				return respuesta.json();
+			})
+			.then((categorias) => {
+				console.log(categorias);
+			});
+	};
+
 	render() {
 		return (
 			<div className="App">

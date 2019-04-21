b99a1cdb4f8baa87a958a2cd6897af90ec339da0
Tito <tito.parra4@@hotmail.com>
2019-04-21 20:43:36 +0200

    Showing the information of the events

--- src/componentes/Evento.js
+++ src/componentes/Evento.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 const Evento = (props) => {
-	return <p>From evento js</p>;
+	const { name } = props.info;
+
+	if (!name) return null;
+
+	let desc = props.info.description.text;
+
+	if (desc.length > 250) {
+		desc = desc.substr(0, 250);
+	}
+
+	return (
+		<div>
+			<div className="uk-card uk-card-default">
+				<div className="uk-card-media-top">
+					{props.info.logo != null ? <img src={props.info.logo.url} alt={props.info.name.text} /> : ''}
+				</div>
+				<div className="uk-card-body">
+					<h3 className="uk-card-title">{props.info.name.text}</h3>
+					<p>{desc}</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Evento;
--- src/componentes/Eventos.js
+++ src/componentes/Eventos.js
@@ -5,7 +5,7 @@ class Eventos extends Component {
 	render() {
 		return (
 			<div className="uk-child-width-1-4@m" uk-grid="true">
-				{Object.keys(this.props.eventos).map((key) => <Evento info={this.props.eventos[key]} />)}
+				{Object.keys(this.props.eventos).map((key) => <Evento key={key} info={this.props.eventos[key]} />)}
 			</div>
 		);
 	}
